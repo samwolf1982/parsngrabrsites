1889c902f641b5d0ce739f8b9c076f70f44fd554
samwolf1982 <biryukwolf@yahoo.com>
2016-04-13 09:49:07 +0300

    не удаляеться бр ????? ст 136

--- index.php
+++ index.php
@@ -30,9 +30,18 @@ function stage1($value='')
 
 $a1='.detailed-advert .skip-export >div:first >';
 $a2='tr.detailed-advert:nth-child(1) > td:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)';
+// addres link
 $a3 ='tr.detailed-advert:nth-child(1) > td:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > span:nth-child(2)';
 
+// district
+// search b and remove
+$a4fordelete ='tr.detailed-advert:nth-child(1) > td:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) b';
+$a4 ='tr.detailed-advert:nth-child(1) > td:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1)';
+
+// search b and remove
 $a345 ='tr.detailed-advert:nth-child(1) > td:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1)';
+
+
 //foto  ???  сейчас показать   взять все сссилки. 
 //tr.detailed-advert:nth-child(1)   менять 1 для подбора
 $a6 ='tr.detailed-advert:nth-child(1) > td:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(2)';
@@ -73,15 +82,71 @@ $p_titleInfo = $document->find($str);
 //Debuger::dumper($p_titleInfo->text());
 $arrElement['link_at_map_adress']=$p_titleInfo->text();
 
-//345   распарсить отдельно. через регульрку
+
+
+
+$a4fordelete ='tr.detailed-advert:nth-child(1) > td:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) b';
+
+$a4  ='tr.detailed-advert:nth-child(1)  td:nth-child(2) div:nth-child(2)  div:nth-child(1)  div:nth-child(1)  div:nth-child(3)  div:nth-child(1):nth-child(1)';
+
+//4 dearch b and remove
+//distrit
 $str=null;
-$str=str_replace('>', '', $a345) ;
+$str=str_replace('>', '', $a4fordelete) ;
+$document->find($str)->remove();      // remove b
+$str=null;
+$str=str_replace('>', '', $a4) ; 
+$str=$a4;       
 $p_titleInfo=null;
-$p_titleInfo = $document->find($str);
+$p_titleInfo = $document->find($str);// find element
 
 //Debuger::dumper($p_titleInfo);
+//var_dump($p_titleInfo);
+//echo "$p_titleInfo->data";
 
-var_dump($p_titleInfo);
+
+
+
+foreach ($p_titleInfo as $key => $value) {
+
+  //echo 'count ' . $value->data;
+/*  foreach ($value->childNodes as $key1 => $value1) {
+    # code...
+    echo $value1;
+  var_dump($value1);
+  echo "<br><br>";
+    //echo $value1->text();
+  }*/
+      //echo  $key;
+ // var_dump($value);
+
+ 
+  
+}
+// echo "$p_titleInfo";*/
+
+
+//$p_titleInfo = $document->find('tr.detailed-advert:nth-child(1) > td:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1)');
+//$s=str_replace('<div class="col-md-6">', '', $s);
+
+$s=null;
+//$s=htmlspecialchars( $p_titleInfo);
+$s=htmlentities ($p_titleInfo,true);
+$s=htmlentities($p_titleInfo, ENT_QUOTES | ENT_IGNORE, "UTF-8",false);
+$s= str_replace('<br />\r<br />', ";", $s);
+//$t="<++>";
+//$s= strtr($s, "<++>", "    ");
+  //$s=explode("<br>",$s); 
+//var_dump( strip_tags ($s));
+//Debuger::dumper($s);
+//$s=str_replace(['<div', 'class="col-md-6">'],' ', $s);
+// $s=strip_tags($s);
+//$pos = strpos($s, ">");
+//substr_replace();
+//$s.chop('<div class="col-md-6">');
+echo $s;
+//echo $pos;
+//echo substr_replace($s,"",0,38);
 
 $arrElement['adress_district_metro_']=$p_titleInfo->text();
 
